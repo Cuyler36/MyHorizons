1887422251f54923fdc8b78427b73564c307459d
Cuyler36 <Cuyler36@users.noreply.github.com>
2020-03-23 00:59:56 -0400

    Implement UI Base
    Uses AvaloniaUI 0.9 for cross-platform support

---
+++ MyHorizons.Avalonia/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+
+namespace MyHorizons.Avalonia
+{
+    public class MainWindow : Window
+    {
+        private Grid TitleBarGrid;
+
+        private Grid CloseGrid;
+        private Button CloseButton;
+
+        private Grid ResizeGrid;
+        private Button ResizeButton;
+
+        private Grid MinimizeGrid;
+        private Button MinimizeButton;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            TitleBarGrid = this.FindControl<Grid>("TitleBarGrid");
+            CloseGrid = this.FindControl<Grid>("CloseGrid");
+            CloseButton = this.FindControl<Button>("CloseButton");
+            ResizeGrid = this.FindControl<Grid>("ResizeGrid");
+            ResizeButton = this.FindControl<Button>("ResizeButton");
+            MinimizeGrid = this.FindControl<Grid>("MinimizeGrid");
+            MinimizeButton = this.FindControl<Button>("MinimizeButton");
+
+            SetupSide("Left", StandardCursorType.LeftSide, WindowEdge.West);
+            SetupSide("Right", StandardCursorType.RightSide, WindowEdge.East);
+            SetupSide("Top", StandardCursorType.TopSide, WindowEdge.North);
+            SetupSide("Bottom", StandardCursorType.BottomSide, WindowEdge.South);
+            SetupSide("TopLeft", StandardCursorType.TopLeftCorner, WindowEdge.NorthWest);
+            SetupSide("TopRight", StandardCursorType.TopRightCorner, WindowEdge.NorthEast);
+            SetupSide("BottomLeft", StandardCursorType.BottomLeftCorner, WindowEdge.SouthWest);
+            SetupSide("BottomRight", StandardCursorType.BottomRightCorner, WindowEdge.SouthEast);
+
+            TitleBarGrid.PointerPressed += (i, e) => PlatformImpl?.BeginMoveDrag(e);
+
+            CloseGrid.PointerEnter += CloseGrid_PointerEnter;
+            CloseGrid.PointerLeave += CloseGrid_PointerLeave;
+
+            ResizeGrid.PointerEnter += ResizeGrid_PointerEnter;
+            ResizeGrid.PointerLeave += ResizeGrid_PointerLeave;
+
+            MinimizeButton.PointerLeave += MinimizeButton_PointerLeave;
+            MinimizeButton.PointerEnter += MinimizeButton_PointerEnter;
+
+            CloseButton.Click += CloseButton_Click;
+            ResizeButton.Click += ResizeButton_Click;
+            MinimizeButton.Click += MinimizeButton_Click;
+
+            PlatformImpl.WindowStateChanged = WindowStateChanged;
+        }
+
+        private void SetupSide(string name, StandardCursorType cursor, WindowEdge edge)
+        {
+            var ctl = this.FindControl<Control>(name);
+            ctl.Cursor = new Cursor(cursor);
+            ctl.PointerPressed += (i, e) => PlatformImpl?.BeginResizeDrag(edge, e);
+        }
+
+        private void WindowStateChanged(WindowState state)
+        {
+            base.HandleWindowStateChanged(state);
+
+            if (state != WindowState.Minimized)
+            {
+                var img = this.FindControl<Image>("ResizeImage");
+                Bitmap bitmap;
+                if (WindowState == WindowState.Normal)
+                {
+                    bitmap = new Bitmap(AvaloniaLocator.Current.GetService<IAssetLoader>().Open(new Uri("resm:MyHorizons.Avalonia.Resources.Maximize.png")));
+                }
+                else
+                {
+                    bitmap = new Bitmap(AvaloniaLocator.Current.GetService<IAssetLoader>().Open(new Uri("resm:MyHorizons.Avalonia.Resources.Restore.png")));
+                }
+
+                img.Source?.Dispose();
+                img.Source = bitmap;
+            }
+        }
+
+        private void CloseButton_Click(object sender, global::Avalonia.Interactivity.RoutedEventArgs e) => Close();
+
+        private void ResizeButton_Click(object sender, global::Avalonia.Interactivity.RoutedEventArgs e)
+            => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        private void MinimizeButton_Click(object sender, global::Avalonia.Interactivity.RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void CloseGrid_PointerEnter(object sender, global::Avalonia.Input.PointerEventArgs e) => CloseGrid.Background = new SolidColorBrush(0xFF648589);
+
+        private void CloseGrid_PointerLeave(object sender, global::Avalonia.Input.PointerEventArgs e) => CloseGrid.Background = Brushes.Transparent;
+
+        private void ResizeGrid_PointerEnter(object sender, global::Avalonia.Input.PointerEventArgs e) => ResizeGrid.Background = new SolidColorBrush(0xFF648589);
+
+        private void ResizeGrid_PointerLeave(object sender, global::Avalonia.Input.PointerEventArgs e) => ResizeGrid.Background = Brushes.Transparent;
+
+        private void MinimizeButton_PointerEnter(object sender, global::Avalonia.Input.PointerEventArgs e) => MinimizeGrid.Background = new SolidColorBrush(0xFF648589);
+
+        private void MinimizeButton_PointerLeave(object sender, global::Avalonia.Input.PointerEventArgs e) => MinimizeGrid.Background = Brushes.Transparent;
+    }
+}
