042c64c7e2a11a2a9a1ab8695989acb46f86584b
igromanru <igromanru@yahoo.de>
2020-04-23 08:45:11 +0200

    added custom Exception for unsupported save version

---
+++ MyHorizons/Exceptions/UnsupportedVersionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyHorizons.Exceptions
+{
+    public class UnsupportedVersionException : Exception
+    {
+        public UnsupportedVersionException()
+        {
+        }
+
+        public UnsupportedVersionException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedVersionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
